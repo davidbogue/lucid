3662ac27daadad36d138cf47b5f2f699cd48bd12
davidbogue <davebogue@gmail.com>
2014-01-24 22:00:01 -0600

    updates to config

--- config/config.go
+++ config/config.go
@@ -3,10 +3,11 @@ package config
 import (
 	"bufio"
 	"os"
+	"strings"
 )
 
 var propertyMap map[string]string
-var propertyFile = string("config.properties")
+var propertyFile = string("./config/config.properties")
 
 func init() {
 	propertyMap = make(map[string]string)
@@ -25,4 +26,15 @@ func init() {
 
 func Value(key string) string {
 	return propertyMap[key]
+}
+
+func startsWith(main string, test string) bool {
+	return strings.Index(main, test) == 0
+}
+
+func splitOnEquals(value string) (string, string) {
+	index := strings.Index(value, "=")
+	key := value[0:index]
+	property := value[index+1:]
+	return key, property
 }
